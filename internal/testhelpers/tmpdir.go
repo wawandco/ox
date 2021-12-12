7e48f614c501d01165632297194c84aa135ed59b
Antonio Pagano <apagano@wawand.co>
2021-12-11 19:24:06 -0500

    passing linter

--- internal/testhelpers/tmpdir.go
+++ internal/testhelpers/tmpdir.go
@@ -10,10 +10,14 @@ func WithinTempDir(t *testing.T, fn func(t *testing.T, dir string)) {
 	t.Helper()
 
 	wd, _ := os.Getwd()
-	defer os.Chdir(wd)
+	defer func() {
+		err := os.Chdir(wd)
+		if err != nil {
+			t.Fatalf("failed to change working directory to %s: %s", wd, err)
+		}
+	}()
 
 	td := t.TempDir()
-
 	err := os.Chdir(td)
 	if err != nil {
 		t.Fatalf("failed to change directory to %s: %s", td, err)
